f10e13402fe2200b1e38404f5f2c5eec859586ef
Kauan <kauaanmatheus@gmail.com>
2026-02-08 09:57:03 -0300

    kickoff

---
+++ src/main/java/com/tcg/pokermon/PokermonApplication.java
@@ -0,0 +1,13 @@
+package com.tcg.pokermon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokermonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokermonApplication.class, args);
+	}
+
+}
